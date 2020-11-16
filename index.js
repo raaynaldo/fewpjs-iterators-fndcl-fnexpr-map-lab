8dd9369560372767486142fae35ad05038a93d14
raaynaldo <raaynaldo@hotmail.com>
2020-11-16 17:46:37 -0500

    commit tree ce8aae104f00f20ae6cc45a6318b9156652c06ba

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(
+    function(sentence){
+      return sentence.split(" ").map(function(word){
+        return word[0].toUpperCase() + word.slice(1)
+      }).join(" ")
+    }
+  );
 }
